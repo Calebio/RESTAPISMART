e66d5ac585d78a60834315d221be98ab10f78dee
Calebio10 <Kalebiotezz@yahoo.com>
2022-01-09 12:37:12 +0100

    Add project files.

---
+++ Data/ApiDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTAPISMART.Data
+{
+    public class ApiDBContext : DbContext
+    {
+
+        public ApiDBContext(DbContextOptions<ApiDBContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Properties> Properties { get; set; }
+        public DbSet<Management> Managements { get; set; }
+
+    }
+}
---
+++ Repository/Repo/ManagementRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTAPI_SMART_EC2_MODEL.Repository.Repo
+{
+    class ManagementRepo
+    {
+    }
+}
