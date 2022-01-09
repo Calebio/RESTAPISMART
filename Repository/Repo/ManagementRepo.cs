60a9719cda58dfc687819d82161ff06ea6f4cdc3
Calebio10 <Kalebiotezz@yahoo.com>
2022-01-09 13:44:10 +0100

    Using dependency injection to link interface and repo

--- Repository/Repo/ManagementRepo.cs
+++ Repository/Repo/ManagementRepo.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using RESTAPISMART.Repository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RESTAPI_SMART_EC2_MODEL.Repository.Repo
+namespace RESTAPISMART.Repository.Repo
 {
-    class ManagementRepo
+    public class ManagementRepo : IManagement
     {
+        public List<IEnumerable<Management>> GetAllManagement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Management> GetManagementByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Management> GetManagementByMarket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Management> GetManagementByName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Management> GetManagementByState()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Repository/Repo/PropertiesRepo.cs
+++ Repository/Repo/PropertiesRepo.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using RESTAPISMART.Repository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RESTAPI_SMART_EC2_MODEL.Repository.Repo
+namespace RESTAPISMART.Repository.Repo
 {
-    class PropertiesRepo
+    public class PropertiesRepo : IProperties
     {
+        public List<IEnumerable<Properties>> GetProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Properties> GetPropertyByFormer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Properties> GetPropertyById(int PropID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Properties> GetPropertyByMarket()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
